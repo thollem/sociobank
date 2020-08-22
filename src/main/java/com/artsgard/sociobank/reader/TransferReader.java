b035d1a65dbc230508ecad39912ff1642dc8f619
thollem <thomaaaas.willeeeem@gmail.com>
2020-08-22 12:01:44 +0200

    problem at date timestamp at FlatFileItemReader

--- src/main/java/com/artsgard/sociobank/reader/TransferReader.java
+++ src/main/java/com/artsgard/sociobank/reader/TransferReader.java
@@ -4,12 +4,14 @@ import com.artsgard.sociobank.model.Account;
 import com.artsgard.sociobank.model.AccountTransfer;
 import java.beans.PropertyEditor;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Map;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Component;
+import sun.util.calendar.BaseCalendar.Date;
 
 /**
  *
@@ -28,7 +30,11 @@ public class TransferReader {
                 .strict(true)
                 .delimited().delimiter(";")
                 .names(new String[]{"accountId", "accountTransferId", "amount", "description", "transferDate"})
-                .targetType(AccountTransfer.class).customEditors((Map<Class<?>, PropertyEditor>) ce)
+                .targetType(AccountTransfer.class).customEditors(Collections.singletonMap(Date.class,
+                    new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), false)))
+                
                 .build();
     }
 }
+
+ 
